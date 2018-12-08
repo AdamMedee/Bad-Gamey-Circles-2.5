2a2107447aded99bb2d26824c33d623cc5546ec3
AdamMehdi <adam_mehdi@hotmail.ca>
2018-12-08 09:50:03 -0500

    main has stuff

--- main.py
+++ main.py
@@ -4,9 +4,45 @@ from pygame import *
 from math import *
 from random import *
 
+init()
+
+WIDTH, HEIGHT = 1080, 720
+
+screen = display.set_mode((WIDTH, HEIGHT))
+
+running = True
+
+leftClick, middleClick, rightClick = False, False, False
+scroll = 0
+mousePos = (0, 0)
+menu = "menu" #menu, help, game, and end
 
 
+while running:
+    leftClick, middleClick, rightClick = False, False, False
+    scroll = 0
+    for action in event.get():
+        if action.type == QUIT:
+            running = False
+            break
 
+    if menu == "menu":
+        pass
+
+    elif menu == "help":
+        pass
+
+    elif menu == "game":
+        pass
+
+    elif menu == "end":
+        pass
+
+    else:
+        print("???")
+
+    display.flip()
+quit()
 
 
 
---
+++ menu.py
@@ -0,0 +1,7 @@
+#diifferent things that can be summoned
+
+from pygame import *
+from math import *
+from random import *
+
+
